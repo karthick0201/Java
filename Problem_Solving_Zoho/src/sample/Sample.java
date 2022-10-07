2e99ff640dbf20534f1518a6913724cd359b2366
R. KARTHICK MUTHU <88630107+karthick0201@users.noreply.github.com>
2022-10-07 17:09:07 +0530

    sample

---
+++ Problem_Solving_Zoho/src/sample/Sample.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("sneha");
+	    scanner.close();
+
+	}
+
+}

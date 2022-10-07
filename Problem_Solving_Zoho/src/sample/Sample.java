4b56d8b24c4cd9bc69f8afee47c7b5cea17243c6
R. KARTHICK MUTHU <88630107+karthick0201@users.noreply.github.com>
2022-10-07 17:10:15 +0530

    sample

--- Problem_Solving_Zoho/src/sample/Sample.java
+++ Problem_Solving_Zoho/src/sample/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("sneha");
+		System.out.println("karthick");
 	    scanner.close();
 
 	}

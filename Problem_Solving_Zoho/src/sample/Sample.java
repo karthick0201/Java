9ccd1264ab1c2424abcfa895fa0e9dbe42ac1711
R. KARTHICK MUTHU <88630107+karthick0201@users.noreply.github.com>
2022-10-08 19:57:23 +0530

    changing Text

--- Problem_Solving_Zoho/src/sample/Sample.java
+++ Problem_Solving_Zoho/src/sample/Sample.java
@@ -7,7 +7,8 @@ public class Sample {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("karthick");
-	    scanner.close();
+		System.out.println("Hi bharath");
+
 
 	}
 
